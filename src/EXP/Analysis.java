7bdbf88ad50815cceb43952feb8936cd69327709
lianghaowei <liang199611@outlook.com>
2016-12-27 17:26:02 +0800

    已经修改成list，三行模式

--- src/EXP/Analysis.java
+++ src/EXP/Analysis.java
@@ -14,16 +14,16 @@ import util.FileWriter;
 public class Analysis {
     private static String[] resultList;
     public static Vector<String> resultVes = new Vector<>();
-    public static LinkedHashMap<String,String> model_compete_Map= new LinkedHashMap<>();
-    public static LinkedHashMap<String,LinkedHashSet<String []>> model_shadow_Map= new LinkedHashMap<String,LinkedHashSet<String[]>> ();
+    public static LinkedHashMap<String, String> model_compete_Map = new LinkedHashMap<>();
+    public static LinkedHashMap<String, LinkedHashSet<String[]>> model_shadow_Map = new LinkedHashMap<String, LinkedHashSet<String[]>>();
 
     /**
      * 分析模式竞争
      * 输出格式
      * 短文本《tab》(模式1,class1)<空格>(模式2,class2)
      */
-    public static void Anylysis_model_compete() throws IOException{
-        FileWriter fw = new FileWriter("Data/Analysis_model_compete.txt", "GBK", false,true);
+    public static void Anylysis_model_compete() throws IOException {
+        FileWriter fw = new FileWriter("Data/Analysis_model_compete.txt", "GBK", false, true);
         /*Stream.of(resultList).
                 map(line -> line.split("\t", -1)).
                 map(strs -> Arrays.asList(strs[2] + "\t" + strs[4], strs[3] + "\t" + strs[5])).
@@ -31,28 +31,28 @@ public class Analysis {
                 distinct().
                 forEach(fw::write);
         */
-        model_compete_Map.entrySet().forEach(x -> fw.write( x.getKey()+"\t"+x.getValue()+"\t"+x.getValue().split("\\) \\(").length+"\n"));
+        model_compete_Map.entrySet().forEach(x -> fw.write(x.getKey() + "\t" + x.getValue() + "\t" + x.getValue().split("\\) \\(").length + "\n"));
         fw.close();
     }
 
     /**
      * 分析模式含混
      * 输入格式：
-     *         line,type
+     * line,type
      * 输出格式
      * 模式<tab>[短句1@class1]<空格>[短句2@class2]...<tab>"kind:"类别数
      */
     public static void Analysis_model_shadow() throws IOException {
-        FileWriter fw = new FileWriter("Data/Analysis_model_shadow.txt", "GBK", false,true);
-        FileWriter fw1 =  new FileWriter("Data/Analysis_model_shadow_greater_than_one.txt", "GBK", false,true);
-        for(Map.Entry<String,LinkedHashSet<String[]>> x:model_shadow_Map.entrySet()){
-            StringBuilder sb =new StringBuilder();
-            List<String> classList= x.getValue().stream().map(strings -> strings[1]).distinct().collect(Collectors.toList());
-            long classCount=classList.size();
-            sb.append(x.getKey()+"\t");
-            x.getValue().forEach(strings -> sb.append("["+strings[0]+"@"+strings[1]+"] "));
-            sb.deleteCharAt(sb.length()-1);
-            sb.append("\t"+classCount+"\t"+classList.toString()+"\n");
+        FileWriter fw = new FileWriter("Data/Analysis_model_shadow.txt", "GBK", false, true);
+        FileWriter fw1 = new FileWriter("Data/Analysis_model_shadow_greater_than_one.txt", "GBK", false, true);
+        for (Map.Entry<String, LinkedHashSet<String[]>> x : model_shadow_Map.entrySet()) {
+            StringBuilder sb = new StringBuilder();
+            List<String> classList = x.getValue().stream().map(strings -> strings[1]).distinct().collect(Collectors.toList());
+            long classCount = classList.size();
+            sb.append(x.getKey() + "\t");
+            x.getValue().forEach(strings -> sb.append("[" + strings[0] + "@" + strings[1] + "] "));
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("\t" + classCount + "\t" + classList.toString() + "\n");
             fw.write(sb.toString());
             if (classCount > 1)
                 fw1.write(sb.toString());
--- src/EXP/Pick.java
+++ src/EXP/Pick.java
@@ -2,20 +2,22 @@ package EXP;
 
 import util.FileWriter;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 
 /**
  * Created by haowei on 2016/12/24.
  */
 public class Pick {
-    private List<String> patternList;
+    private List<List<String>> patternList;
     private String patternFilename, inputFilename, outputFilename;
 
     private Pick(String patternFilename, String inputFilename, String outputFilename) {
@@ -25,35 +27,48 @@ public class Pick {
     }
 
     private void loadPattern() throws IOException {
+        patternList = new ArrayList<List<String>>();
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(patternFilename), "GBK"));
-        patternList = br.
-                lines().
-                map(line -> line.replaceAll("\\.\\*", "(?<keySentence>.*?)")).
-                collect(Collectors.toList());
+        List<String> lines = br.lines().collect(Collectors.toList());
+        for (int i = 0; i < lines.size() / 3; i++) {
+            List<String> regexs = new ArrayList<String>();
+            String line1, line2, line3;
+            line1 = lines.get(3 * i);
+            line2 = lines.get(3 * i + 1);
+            line3 = lines.get(3 * i + 2);
+            for (String left : line1.split("\t"))
+                for (String right : line2.split("\t"))
+                    regexs.add("(" + left + ")" + ".*" + "(" + right + ")");
+            Arrays.stream(line3.split("\t")).filter(string -> string.length()!=0).forEach(regex -> regexs.add(regex));
+            patternList.add(regexs.stream().map(regex -> regex.replaceAll("\\.\\*", "(?<keySentence>.*?)")).
+                    collect(Collectors.toList()));
+        }
         br.close();
-        System.out.print("debug" + patternList.toString());
+        patternList.forEach(regexs -> regexs.forEach(System.out::println));
     }
 
     private String isKeyType(String regex, String line) {
         Matcher pa = Pattern.compile(regex).matcher(line);
         if (!pa.find())
             return "";
-        if (pa.group("keySentence").length() == 0){
+        if (pa.group("keySentence").length() == 0) {
             StringBuilder sb = new StringBuilder(pa.group(0));
-            sb.insert(pa.start("keySentence")-pa.start(0),"\t\t");
+            sb.insert(pa.start("keySentence") - pa.start(0), "\t\t");
             return sb.toString();
         }
-        System.out.println("debug: " + pa.group(0));
-        return Pattern.compile(pa.group("keySentence"), Pattern.LITERAL).matcher(pa.group(0)).replaceAll("\t" + pa.group("keySentence") + "\t");
+        //System.out.println("debug: " + pa.group(0));
+        return Pattern.compile(pa.group("keySentence"), Pattern.LITERAL).
+                matcher(pa.group(0)).
+                replaceAll("\t" + pa.group("keySentence") + "\t");
     }
 
     private String type(String line) {
         String result = patternList.
                 stream().
-                map(strings -> isKeyType(strings, line)).
-                distinct().
-                filter(x -> x.length() != 0).
-                reduce("", (a, b) -> a + "|" + b);
+                map(regexs -> regexs.stream().map(regex -> isKeyType(regex, line)).
+                        filter(strings -> strings.length() != 0).
+                        findFirst().orElse("")).
+                reduce("", (a, b) -> a + "\t" + b);
         if (result.length() > 0)
             result = result.substring(1);
         return result;
